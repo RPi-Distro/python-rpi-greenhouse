111cf6e6cccacdc361d71115ea347d3eb2f4f0f1
Ben Nuttall <ben@bennuttall.com>
2015-06-12 19:56:24 +0100

    Implement dummy sensor properties

--- greenhouse/greenhouse.py
+++ greenhouse/greenhouse.py
@@ -5,28 +5,74 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 class Greenhouse(object):
-    leds = {
+    LEDS = {
         'red': [16, 11, 23],
         'white': [13, 9, 27],
         'green': [21, 12, 25],
         'blue': [20, 6, 22],
     }
 
+    target_temperature_lower = 5
+    target_temperature_upper = 30
+
+    target_humidity_lower = 0.3
+    target_humidity_upper = 0.6
+
+    target_soil = 0.6
+
+    target_light = 0.6
+
+    @property
+    def temperature(self):
+        return 30.0
+
+    @property
+    def humidity(self):
+        return 0.4
+
+    @property
+    def soil(self):
+        return 0.6
+
+    @property
+    def light(self):
+        return 0.8
+
+    @property
+    def temperature_status(self):
+        lower = self.target_temperature_lower
+        upper = self.target_temperature_upper
+        return lower <= self.temperature <= upper
+
+    @property
+    def humidity_status(self):
+        lower = self.target_humidity_lower
+        upper = self.target_humidity_upper
+        return lower <= self.humidity <= upper
+
+    @property
+    def soil_status(self):
+        return self.soil >= self.target_soil
+
+    @property
+    def light_status(self):
+        return self.light >= self.target_light
+
     def __init__(self):
         self._setup_gpio()
 
     def _setup_gpio(self):
-        for colour in self.leds:
-            for led in self.leds[colour]:
+        for colour in self.LEDS:
+            for led in self.LEDS[colour]:
                 GPIO.setup(led, GPIO.OUT)
                 GPIO.output(led, False)
 
     def _turn_led_on_or_off(self, colour, index, on_or_off):
-        led = self.leds[colour][index]
+        led = self.LEDS[colour][index]
         GPIO.output(led, on_or_off)
 
     def _turn_colour_leds_on_or_off(self, colour, on_or_off):
-        leds = self.leds[colour]
+        leds = self.LEDS[colour]
         for led in range(len(leds)):
             if on_or_off:
                 self.turn_led_on(colour, led)
@@ -34,7 +80,7 @@ class Greenhouse(object):
                 self.turn_led_off(colour, led)
 
     def _turn_all_leds_on_or_off(self, on_or_off):
-        for colour in self.leds:
+        for colour in self.LEDS:
             if on_or_off:
                 self.turn_colour_leds_on(colour)
             else:
@@ -86,14 +132,22 @@ class Greenhouse(object):
 
 def main():
     greenhouse = Greenhouse()
-    greenhouse.turn_all_leds_on()
-    sleep(2)
-    greenhouse.turn_all_leds_off()
-    sleep(2)
-    for colour in greenhouse.leds:
-        greenhouse.turn_colour_leds_on(colour)
-        sleep(2)
-        greenhouse.turn_colour_leds_off(colour)
+    if greenhouse.temperature_status:
+        print("Temperature ok")
+    else:
+        print("Temperature not ok")
+    if greenhouse.humidity_status:
+        print("Humidity ok")
+    else:
+        print("Humidity not ok")
+    if greenhouse.soil_status:
+        print("Soil ok")
+    else:
+        print("Soil not ok")
+    if greenhouse.light_status:
+        print("Light ok")
+    else:
+        print("Light not ok")
 
 if __name__ == '__main__':
     main()
